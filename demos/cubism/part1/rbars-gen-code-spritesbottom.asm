9d1a5f02b2c7883e458afb5d299ec4a1a89be2a9
Claus <pruefer@webcodex.de>
2025-07-28 11:45:22 +0200

    Add generated sprites processing code

---
+++ demos/cubism/part1/rbars-gen-code-spritesbottom.asm
@@ -0,0 +1,1124 @@
+; adjusted timing
+
+nop
+nop
+nop
+nop
+nop
+
+; sprites x position #1 -  template start
+
+lbl_barcol21
+  lda #0
+
+lbl_sprite21_1
+  ldx #0
+  ldy #0
+
+  stx $d000
+  sty $d002
+
+lbl_sprite21_2
+  ldx #0
+  ldy #0
+
+  stx $d004
+  sty $d006
+
+  sta $d020
+  sta $d021
+
+; raster col line #1
+
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+
+; sprites x position #2
+
+lbl_sprite22_1
+  ldx #0
+  ldy #0
+
+  stx $d000
+  sty $d002
+
+lbl_sprite22_2
+  ldx #0
+  ldy #0
+
+  stx $d004
+  sty $d006
+
+; raster col line #2
+
+lbl_barcol22
+  lda #0
+  sta $d020
+  sta $d021
+
+; timing
+
+  nop
+  nop
+  nop
+  nop
+  nop
+
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+
+; sprites x position #3
+
+lbl_sprite23_1
+  ldx #0
+  ldy #0
+
+  stx $d000
+  sty $d002
+
+lbl_sprite23_2
+  ldx #0
+  ldy #0
+
+  stx $d004
+  sty $d006
+
+; raster col line #3
+
+lbl_barcol23
+  lda #0
+  sta $d020
+  sta $d021
+
+; timing
+
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+
+; sprites x position #4
+
+lbl_sprite24_1
+  ldx #0
+  ldy #0
+
+  stx $d004
+  sty $d006
+
+lbl_sprite24_2
+  ldx #0
+  ldy #0
+
+  stx $d000
+  sty $d002
+
+; raster col line #4
+
+lbl_barcol24
+  lda #0
+  sta $d020
+  sta $d021
+
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+; sprites x position #1 -  template start
+
+lbl_barcol25
+  lda #0
+
+lbl_sprite25_1
+  ldx #0
+  ldy #0
+
+  stx $d000
+  sty $d002
+
+lbl_sprite25_2
+  ldx #0
+  ldy #0
+
+  stx $d004
+  sty $d006
+
+  sta $d021
+  sta $d020
+
+; raster col line #1
+
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+
+; sprites x position #2
+
+lbl_sprite26_1
+  ldx #0
+  ldy #0
+
+  stx $d000
+  sty $d002
+
+lbl_sprite26_2
+  ldx #0
+  ldy #0
+
+  stx $d004
+  sty $d006
+
+; raster col line #2
+
+lbl_barcol26
+  lda #0
+  sta $d020
+  sta $d021
+
+; timing
+
+  nop
+  nop
+  nop
+  nop
+  nop
+
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+
+; sprites x position #3
+
+lbl_sprite27_1
+  ldx #0
+  ldy #0
+
+  stx $d000
+  sty $d002
+
+lbl_sprite27_2
+  ldx #0
+  ldy #0
+
+  stx $d004
+  sty $d006
+
+; raster col line #3
+
+lbl_barcol27
+  lda #0
+  sta $d020
+  sta $d021
+
+; timing
+
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+
+; sprites x position #4
+
+lbl_sprite28_1
+  ldx #0
+  ldy #0
+
+  stx $d004
+  sty $d006
+
+lbl_sprite28_2
+  ldx #0
+  ldy #0
+
+  stx $d000
+  sty $d002
+
+; raster col line #4
+
+lbl_barcol28
+  lda #0
+  sta $d020
+  sta $d021
+
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+; sprites x position #1 -  template start
+
+lbl_barcol29
+  lda #0
+
+lbl_sprite29_1
+  ldx #0
+  ldy #0
+
+  stx $d000
+  sty $d002
+
+lbl_sprite29_2
+  ldx #0
+  ldy #0
+
+  stx $d004
+  sty $d006
+
+  sta $d021
+  sta $d020
+
+; raster col line #1
+
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+
+; sprites x position #2
+
+lbl_sprite30_1
+  ldx #0
+  ldy #0
+
+  stx $d000
+  sty $d002
+
+lbl_sprite30_2
+  ldx #0
+  ldy #0
+
+  stx $d004
+  sty $d006
+
+; raster col line #2
+
+lbl_barcol30
+  lda #0
+  sta $d020
+  sta $d021
+
+; timing
+
+  nop
+  nop
+  nop
+  nop
+  nop
+
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+
+; sprites x position #3
+
+lbl_sprite31_1
+  ldx #0
+  ldy #0
+
+  stx $d000
+  sty $d002
+
+lbl_sprite31_2
+  ldx #0
+  ldy #0
+
+  stx $d004
+  sty $d006
+
+; raster col line #3
+
+lbl_barcol31
+  lda #0
+  sta $d020
+  sta $d021
+
+; timing
+
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+
+; sprites x position #4
+
+lbl_sprite32_1
+  ldx #0
+  ldy #0
+
+  stx $d004
+  sty $d006
+
+lbl_sprite32_2
+  ldx #0
+  ldy #0
+
+  stx $d000
+  sty $d002
+
+; raster col line #4
+
+lbl_barcol32
+  lda #0
+  sta $d020
+  sta $d021
+
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+; sprites x position #1 -  template start
+
+lbl_barcol33
+  lda #0
+
+lbl_sprite33_1
+  ldx #0
+  ldy #0
+
+  stx $d000
+  sty $d002
+
+lbl_sprite33_2
+  ldx #0
+  ldy #0
+
+  stx $d004
+  sty $d006
+
+  sta $d021
+  sta $d020
+
+; raster col line #1
+
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+
+; sprites x position #2
+
+lbl_sprite34_1
+  ldx #0
+  ldy #0
+
+  stx $d000
+  sty $d002
+
+lbl_sprite34_2
+  ldx #0
+  ldy #0
+
+  stx $d004
+  sty $d006
+
+; raster col line #2
+
+lbl_barcol34
+  lda #0
+  sta $d020
+  sta $d021
+
+; timing
+
+  nop
+  nop
+  nop
+  nop
+  nop
+
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+
+; sprites x position #3
+
+lbl_sprite35_1
+  ldx #0
+  ldy #0
+
+  stx $d000
+  sty $d002
+
+lbl_sprite35_2
+  ldx #0
+  ldy #0
+
+  stx $d004
+  sty $d006
+
+; raster col line #3
+
+lbl_barcol35
+  lda #0
+  sta $d020
+  sta $d021
+
+; timing
+
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+
+; sprites x position #4
+
+lbl_sprite36_1
+  ldx #0
+  ldy #0
+
+  stx $d004
+  sty $d006
+
+lbl_sprite36_2
+  ldx #0
+  ldy #0
+
+  stx $d000
+  sty $d002
+
+; raster col line #4
+
+lbl_barcol36
+  lda #0
+  sta $d020
+  sta $d021
+
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+; sprites x position #1 -  template start
+
+lbl_barcol37
+  lda #0
+
+lbl_sprite37_1
+  ldx #0
+  ldy #0
+
+  stx $d000
+  sty $d002
+
+lbl_sprite37_2
+  ldx #0
+  ldy #0
+
+  stx $d004
+  sty $d006
+
+  sta $d021
+  sta $d020
+
+; raster col line #1
+
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+
+; sprites x position #2
+
+lbl_sprite38_1
+  ldx #0
+  ldy #0
+
+  stx $d000
+  sty $d002
+
+lbl_sprite38_2
+  ldx #0
+  ldy #0
+
+  stx $d004
+  sty $d006
+
+; raster col line #2
+
+lbl_barcol38
+  lda #0
+  sta $d020
+  sta $d021
+
+; timing
+
+  nop
+  nop
+  nop
+  nop
+  nop
+
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+
+; sprites x position #3
+
+lbl_sprite39_1
+  ldx #0
+  ldy #0
+
+  stx $d000
+  sty $d002
+
+lbl_sprite39_2
+  ldx #0
+  ldy #0
+
+  stx $d004
+  sty $d006
+
+; raster col line #3
+
+lbl_barcol39
+  lda #0
+  sta $d020
+  sta $d021
+
+; timing
+
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+
+; sprites x position #4
+
+lbl_sprite40_1
+  ldx #0
+  ldy #0
+
+  stx $d004
+  sty $d006
+
+lbl_sprite40_2
+  ldx #0
+  ldy #0
+
+  stx $d000
+  sty $d002
+
+; raster col last line
+
+lbl_barcol40
+
+  lda #1
+  sta $d020
+  sta $d021
+
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
---
+++ demos/cubism/part1/rbars-gen-code-spritestop.asm
@@ -0,0 +1,901 @@
+; sprites x position #1 -  template start
+
+lbl_barcol5
+  lda #0
+
+lbl_sprite5_1
+  ldx #0
+  ldy #0
+
+  stx $d000
+  sty $d002
+
+lbl_sprite5_2
+  ldx #0
+  ldy #0
+
+  stx $d004
+  sty $d006
+
+  sta $d021
+  sta $d020
+
+; raster col line #1
+
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+
+; sprites x position #2
+
+lbl_sprite6_1
+  ldx #0
+  ldy #0
+
+  stx $d000
+  sty $d002
+
+lbl_sprite6_2
+  ldx #0
+  ldy #0
+
+  stx $d004
+  sty $d006
+
+; raster col line #2
+
+lbl_barcol6
+  lda #0
+  sta $d020
+  sta $d021
+
+; timing
+
+  nop
+  nop
+  nop
+  nop
+  nop
+
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+
+; sprites x position #3
+
+lbl_sprite7_1
+  ldx #0
+  ldy #0
+
+  stx $d000
+  sty $d002
+
+lbl_sprite7_2
+  ldx #0
+  ldy #0
+
+  stx $d004
+  sty $d006
+
+; raster col line #3
+
+lbl_barcol7
+  lda #0
+  sta $d020
+  sta $d021
+
+; timing
+
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+
+; sprites x position #4
+
+lbl_sprite8_1
+  ldx #0
+  ldy #0
+
+  stx $d004
+  sty $d006
+
+lbl_sprite8_2
+  ldx #0
+  ldy #0
+
+  stx $d000
+  sty $d002
+
+; raster col line #4
+
+lbl_barcol8
+  lda #0
+  sta $d020
+  sta $d021
+
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+; sprites x position #1 -  template start
+
+lbl_barcol9
+  lda #0
+
+lbl_sprite9_1
+  ldx #0
+  ldy #0
+
+  stx $d000
+  sty $d002
+
+lbl_sprite9_2
+  ldx #0
+  ldy #0
+
+  stx $d004
+  sty $d006
+
+  sta $d021
+  sta $d020
+
+; raster col line #1
+
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+
+; sprites x position #2
+
+lbl_sprite10_1
+  ldx #0
+  ldy #0
+
+  stx $d000
+  sty $d002
+
+lbl_sprite10_2
+  ldx #0
+  ldy #0
+
+  stx $d004
+  sty $d006
+
+; raster col line #2
+
+lbl_barcol10
+  lda #0
+  sta $d020
+  sta $d021
+
+; timing
+
+  nop
+  nop
+  nop
+  nop
+  nop
+
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+
+; sprites x position #3
+
+lbl_sprite11_1
+  ldx #0
+  ldy #0
+
+  stx $d000
+  sty $d002
+
+lbl_sprite11_2
+  ldx #0
+  ldy #0
+
+  stx $d004
+  sty $d006
+
+; raster col line #3
+
+lbl_barcol11
+  lda #0
+  sta $d020
+  sta $d021
+
+; timing
+
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+
+; sprites x position #4
+
+lbl_sprite12_1
+  ldx #0
+  ldy #0
+
+  stx $d004
+  sty $d006
+
+lbl_sprite12_2
+  ldx #0
+  ldy #0
+
+  stx $d000
+  sty $d002
+
+; raster col line #4
+
+lbl_barcol12
+  lda #0
+  sta $d020
+  sta $d021
+
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+; sprites x position #1 -  template start
+
+lbl_barcol13
+  lda #0
+
+lbl_sprite13_1
+  ldx #0
+  ldy #0
+
+  stx $d000
+  sty $d002
+
+lbl_sprite13_2
+  ldx #0
+  ldy #0
+
+  stx $d004
+  sty $d006
+
+  sta $d021
+  sta $d020
+
+; raster col line #1
+
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+
+; sprites x position #2
+
+lbl_sprite14_1
+  ldx #0
+  ldy #0
+
+  stx $d000
+  sty $d002
+
+lbl_sprite14_2
+  ldx #0
+  ldy #0
+
+  stx $d004
+  sty $d006
+
+; raster col line #2
+
+lbl_barcol14
+  lda #0
+  sta $d020
+  sta $d021
+
+; timing
+
+  nop
+  nop
+  nop
+  nop
+  nop
+
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+
+; sprites x position #3
+
+lbl_sprite15_1
+  ldx #0
+  ldy #0
+
+  stx $d000
+  sty $d002
+
+lbl_sprite15_2
+  ldx #0
+  ldy #0
+
+  stx $d004
+  sty $d006
+
+; raster col line #3
+
+lbl_barcol15
+  lda #0
+  sta $d020
+  sta $d021
+
+; timing
+
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+
+; sprites x position #4
+
+lbl_sprite16_1
+  ldx #0
+  ldy #0
+
+  stx $d004
+  sty $d006
+
+lbl_sprite16_2
+  ldx #0
+  ldy #0
+
+  stx $d000
+  sty $d002
+
+; raster col line #4
+
+lbl_barcol16
+  lda #0
+  sta $d020
+  sta $d021
+
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+; sprites x position #1 -  template start
+
+lbl_barcol17
+  lda #0
+
+lbl_sprite17_1
+  ldx #0
+  ldy #0
+
+  stx $d000
+  sty $d002
+
+lbl_sprite17_2
+  ldx #0
+  ldy #0
+
+  stx $d004
+  sty $d006
+
+  sta $d021
+  sta $d020
+
+; raster col line #1
+
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+
+; sprites x position #2
+
+lbl_sprite18_1
+  ldx #0
+  ldy #0
+
+  stx $d000
+  sty $d002
+
+lbl_sprite18_2
+  ldx #0
+  ldy #0
+
+  stx $d004
+  sty $d006
+
+; raster col line #2
+
+lbl_barcol18
+  lda #0
+  sta $d020
+  sta $d021
+
+; timing
+
+  nop
+  nop
+  nop
+  nop
+  nop
+
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+
+; sprites x position #3
+
+lbl_sprite19_1
+  ldx #0
+  ldy #0
+
+  stx $d000
+  sty $d002
+
+lbl_sprite19_2
+  ldx #0
+  ldy #0
+
+  stx $d004
+  sty $d006
+
+; raster col line #3
+
+lbl_barcol19
+  lda #0
+  sta $d020
+  sta $d021
+
+; timing
+
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+
+; sprites x position #4
+
+lbl_sprite20_1
+  ldx #0
+  ldy #0
+
+  stx $d004
+  sty $d006
+
+lbl_sprite20_2
+  ldx #0
+  ldy #0
+
+  stx $d000
+  sty $d002
+
+; raster col line #4
+
+lbl_barcol20
+  lda #0
+  sta $d020
+  sta $d021
+
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+
+  nop
+  nop
+  nop
