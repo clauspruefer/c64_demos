35648bc7df54b815620e49910db41c78ec50626a
Claus <pruefer@webcodex.de>
2025-07-28 12:13:08 +0200

    Add data processing .asm includes (generated code)

---
+++ demos/cubism/part2/rasterline-opcodes-out.asm
@@ -0,0 +1,2061 @@
+charset_map_offset0
+  lda #%00100000
+  sta $d018
+scroll_offset0
+  lda #0
+  sta $d016
+background_color0
+  lda #$0b
+  sta $d021
+  nop
+  nop
+  nop
+charset_map_offset1
+  lda #%00100000
+  sta $d018
+scroll_offset1
+  lda #0
+  sta $d016
+background_color1
+  lda #$0b
+  sta $d021
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+charset_map_offset2
+  lda #%00100000
+  sta $d018
+scroll_offset2
+  lda #0
+  sta $d016
+background_color2
+  lda #$0f
+  sta $d021
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+charset_map_offset3
+  lda #%00100000
+  sta $d018
+scroll_offset3
+  lda #0
+  sta $d016
+background_color3
+  lda #$0b
+  sta $d021
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+charset_map_offset4
+  lda #%00100000
+  sta $d018
+scroll_offset4
+  lda #0
+  sta $d016
+background_color4
+  lda #$0b
+  sta $d021
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+charset_map_offset5
+  lda #%00100000
+  sta $d018
+scroll_offset5
+  lda #0
+  sta $d016
+background_color5
+  lda #$0b
+  sta $d021
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+charset_map_offset6
+  lda #%00100000
+  sta $d018
+scroll_offset6
+  lda #0
+  sta $d016
+background_color6
+  lda #$0f
+  sta $d021
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+charset_map_offset7
+  lda #%00100000
+  sta $d018
+scroll_offset7
+  lda #0
+  sta $d016
+background_color7
+  lda #$0b
+  sta $d021
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+charset_map_offset8
+  lda #%00100000
+  sta $d018
+scroll_offset8
+  lda #0
+  sta $d016
+background_color8
+  lda #$0b
+  sta $d021
+  nop
+  nop
+  nop
+charset_map_offset9
+  lda #%00100000
+  sta $d018
+scroll_offset9
+  lda #0
+  sta $d016
+background_color9
+  lda #$0f
+  sta $d021
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+charset_map_offset10
+  lda #%00100000
+  sta $d018
+scroll_offset10
+  lda #0
+  sta $d016
+background_color10
+  lda #$0f
+  sta $d021
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+charset_map_offset11
+  lda #%00100000
+  sta $d018
+scroll_offset11
+  lda #0
+  sta $d016
+background_color11
+  lda #$0b
+  sta $d021
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+charset_map_offset12
+  lda #%00100000
+  sta $d018
+scroll_offset12
+  lda #0
+  sta $d016
+background_color12
+  lda #$0b
+  sta $d021
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+charset_map_offset13
+  lda #%00100000
+  sta $d018
+scroll_offset13
+  lda #0
+  sta $d016
+background_color13
+  lda #$0b
+  sta $d021
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+charset_map_offset14
+  lda #%00100000
+  sta $d018
+scroll_offset14
+  lda #0
+  sta $d016
+background_color14
+  lda #$0b
+  sta $d021
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+charset_map_offset15
+  lda #%00100000
+  sta $d018
+scroll_offset15
+  lda #0
+  sta $d016
+background_color15
+  lda #$0b
+  sta $d021
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+charset_map_offset16
+  lda #%00100000
+  sta $d018
+scroll_offset16
+  lda #0
+  sta $d016
+background_color16
+  lda #$0b
+  sta $d021
+  nop
+  nop
+  nop
+charset_map_offset17
+  lda #%00100000
+  sta $d018
+scroll_offset17
+  lda #0
+  sta $d016
+background_color17
+  lda #$0b
+  sta $d021
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+charset_map_offset18
+  lda #%00100000
+  sta $d018
+scroll_offset18
+  lda #0
+  sta $d016
+background_color18
+  lda #$0b
+  sta $d021
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+charset_map_offset19
+  lda #%00100000
+  sta $d018
+scroll_offset19
+  lda #0
+  sta $d016
+background_color19
+  lda #$0b
+  sta $d021
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+charset_map_offset20
+  lda #%00100000
+  sta $d018
+scroll_offset20
+  lda #0
+  sta $d016
+background_color20
+  lda #$0b
+  sta $d021
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+charset_map_offset21
+  lda #%00100000
+  sta $d018
+scroll_offset21
+  lda #0
+  sta $d016
+background_color21
+  lda #$0b
+  sta $d021
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+charset_map_offset22
+  lda #%00100000
+  sta $d018
+scroll_offset22
+  lda #0
+  sta $d016
+background_color22
+  lda #$0b
+  sta $d021
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+charset_map_offset23
+  lda #%00100000
+  sta $d018
+scroll_offset23
+  lda #0
+  sta $d016
+background_color23
+  lda #$0b
+  sta $d021
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+charset_map_offset24
+  lda #%00100000
+  sta $d018
+scroll_offset24
+  lda #0
+  sta $d016
+background_color24
+  lda #$0b
+  sta $d021
+  nop
+  nop
+  nop
+charset_map_offset25
+  lda #%00100000
+  sta $d018
+scroll_offset25
+  lda #0
+  sta $d016
+background_color25
+  lda #$0b
+  sta $d021
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+charset_map_offset26
+  lda #%00100000
+  sta $d018
+scroll_offset26
+  lda #0
+  sta $d016
+background_color26
+  lda #$0b
+  sta $d021
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+charset_map_offset27
+  lda #%00100000
+  sta $d018
+scroll_offset27
+  lda #0
+  sta $d016
+background_color27
+  lda #$0b
+  sta $d021
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+charset_map_offset28
+  lda #%00100000
+  sta $d018
+scroll_offset28
+  lda #0
+  sta $d016
+background_color28
+  lda #$0b
+  sta $d021
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+charset_map_offset29
+  lda #%00100000
+  sta $d018
+scroll_offset29
+  lda #0
+  sta $d016
+background_color29
+  lda #$0b
+  sta $d021
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+charset_map_offset30
+  lda #%00100000
+  sta $d018
+scroll_offset30
+  lda #0
+  sta $d016
+background_color30
+  lda #$0b
+  sta $d021
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+charset_map_offset31
+  lda #%00100000
+  sta $d018
+scroll_offset31
+  lda #0
+  sta $d016
+background_color31
+  lda #$0b
+  sta $d021
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+charset_map_offset32
+  lda #%00100000
+  sta $d018
+scroll_offset32
+  lda #0
+  sta $d016
+background_color32
+  lda #$0b
+  sta $d021
+  nop
+  nop
+  nop
+charset_map_offset33
+  lda #%00100000
+  sta $d018
+scroll_offset33
+  lda #0
+  sta $d016
+background_color33
+  lda #$0b
+  sta $d021
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+charset_map_offset34
+  lda #%00100000
+  sta $d018
+scroll_offset34
+  lda #0
+  sta $d016
+background_color34
+  lda #$0b
+  sta $d021
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+charset_map_offset35
+  lda #%00100000
+  sta $d018
+scroll_offset35
+  lda #0
+  sta $d016
+background_color35
+  lda #$0b
+  sta $d021
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+charset_map_offset36
+  lda #%00100000
+  sta $d018
+scroll_offset36
+  lda #0
+  sta $d016
+background_color36
+  lda #$0b
+  sta $d021
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+charset_map_offset37
+  lda #%00100000
+  sta $d018
+scroll_offset37
+  lda #0
+  sta $d016
+background_color37
+  lda #$0b
+  sta $d021
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+charset_map_offset38
+  lda #%00100000
+  sta $d018
+scroll_offset38
+  lda #0
+  sta $d016
+background_color38
+  lda #$0b
+  sta $d021
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+charset_map_offset39
+  lda #%00100000
+  sta $d018
+scroll_offset39
+  lda #0
+  sta $d016
+background_color39
+  lda #$0b
+  sta $d021
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+charset_map_offset40
+  lda #%00100000
+  sta $d018
+scroll_offset40
+  lda #0
+  sta $d016
+background_color40
+  lda #$0b
+  sta $d021
+  nop
+  nop
+  nop
+charset_map_offset41
+  lda #%00100000
+  sta $d018
+scroll_offset41
+  lda #0
+  sta $d016
+background_color41
+  lda #$0b
+  sta $d021
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+charset_map_offset42
+  lda #%00100000
+  sta $d018
+scroll_offset42
+  lda #0
+  sta $d016
+background_color42
+  lda #$0b
+  sta $d021
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+charset_map_offset43
+  lda #%00100000
+  sta $d018
+scroll_offset43
+  lda #0
+  sta $d016
+background_color43
+  lda #$0b
+  sta $d021
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+charset_map_offset44
+  lda #%00100000
+  sta $d018
+scroll_offset44
+  lda #0
+  sta $d016
+background_color44
+  lda #$0b
+  sta $d021
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+charset_map_offset45
+  lda #%00100000
+  sta $d018
+scroll_offset45
+  lda #0
+  sta $d016
+background_color45
+  lda #$0b
+  sta $d021
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+charset_map_offset46
+  lda #%00100000
+  sta $d018
+scroll_offset46
+  lda #0
+  sta $d016
+background_color46
+  lda #$0b
+  sta $d021
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+charset_map_offset47
+  lda #%00100000
+  sta $d018
+scroll_offset47
+  lda #0
+  sta $d016
+background_color47
+  lda #$0b
+  sta $d021
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+charset_map_offset48
+  lda #%00100000
+  sta $d018
+scroll_offset48
+  lda #0
+  sta $d016
+background_color48
+  lda #$0b
+  sta $d021
+  nop
+  nop
+  nop
+charset_map_offset49
+  lda #%00100000
+  sta $d018
+scroll_offset49
+  lda #0
+  sta $d016
+background_color49
+  lda #$0b
+  sta $d021
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+charset_map_offset50
+  lda #%00100000
+  sta $d018
+scroll_offset50
+  lda #0
+  sta $d016
+background_color50
+  lda #$0b
+  sta $d021
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+charset_map_offset51
+  lda #%00100000
+  sta $d018
+scroll_offset51
+  lda #0
+  sta $d016
+background_color51
+  lda #$0b
+  sta $d021
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+charset_map_offset52
+  lda #%00100000
+  sta $d018
+scroll_offset52
+  lda #0
+  sta $d016
+background_color52
+  lda #$0b
+  sta $d021
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+charset_map_offset53
+  lda #%00100000
+  sta $d018
+scroll_offset53
+  lda #0
+  sta $d016
+background_color53
+  lda #$0b
+  sta $d021
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+charset_map_offset54
+  lda #%00100000
+  sta $d018
+scroll_offset54
+  lda #0
+  sta $d016
+background_color54
+  lda #$0b
+  sta $d021
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+charset_map_offset55
+  lda #%00100000
+  sta $d018
+scroll_offset55
+  lda #0
+  sta $d016
+background_color55
+  lda #$0b
+  sta $d021
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+charset_map_offset56
+  lda #%00100000
+  sta $d018
+scroll_offset56
+  lda #0
+  sta $d016
+background_color56
+  lda #$0b
+  sta $d021
+  nop
+  nop
+  nop
+charset_map_offset57
+  lda #%00100000
+  sta $d018
+scroll_offset57
+  lda #0
+  sta $d016
+background_color57
+  lda #$0b
+  sta $d021
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+charset_map_offset58
+  lda #%00100000
+  sta $d018
+scroll_offset58
+  lda #0
+  sta $d016
+background_color58
+  lda #$0b
+  sta $d021
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+charset_map_offset59
+  lda #%00100000
+  sta $d018
+scroll_offset59
+  lda #0
+  sta $d016
+background_color59
+  lda #$0b
+  sta $d021
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+charset_map_offset60
+  lda #%00100000
+  sta $d018
+scroll_offset60
+  lda #0
+  sta $d016
+background_color60
+  lda #$0b
+  sta $d021
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+charset_map_offset61
+  lda #%00100000
+  sta $d018
+scroll_offset61
+  lda #0
+  sta $d016
+background_color61
+  lda #$0b
+  sta $d021
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+charset_map_offset62
+  lda #%00100000
+  sta $d018
+scroll_offset62
+  lda #0
+  sta $d016
+background_color62
+  lda #$0b
+  sta $d021
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+charset_map_offset63
+  lda #%00100000
+  sta $d018
+scroll_offset63
+  lda #0
+  sta $d016
+background_color63
+  lda #$0b
+  sta $d021
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+charset_map_offset64
+  lda #%00100000
+  sta $d018
+scroll_offset64
+  lda #0
+  sta $d016
+background_color64
+  lda #$0b
+  sta $d021
+  nop
+  nop
+  nop
+charset_map_offset65
+  lda #%00100000
+  sta $d018
+scroll_offset65
+  lda #0
+  sta $d016
+background_color65
+  lda #$0b
+  sta $d021
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+charset_map_offset66
+  lda #%00100000
+  sta $d018
+scroll_offset66
+  lda #0
+  sta $d016
+background_color66
+  lda #$0b
+  sta $d021
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+charset_map_offset67
+  lda #%00100000
+  sta $d018
+scroll_offset67
+  lda #0
+  sta $d016
+background_color67
+  lda #$0b
+  sta $d021
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+charset_map_offset68
+  lda #%00100000
+  sta $d018
+scroll_offset68
+  lda #0
+  sta $d016
+background_color68
+  lda #$0b
+  sta $d021
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+charset_map_offset69
+  lda #%00100000
+  sta $d018
+scroll_offset69
+  lda #0
+  sta $d016
+background_color69
+  lda #$0b
+  sta $d021
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+charset_map_offset70
+  lda #%00100000
+  sta $d018
+scroll_offset70
+  lda #0
+  sta $d016
+background_color70
+  lda #$0b
+  sta $d021
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+charset_map_offset71
+  lda #%00100000
+  sta $d018
+scroll_offset71
+  lda #0
+  sta $d016
+background_color71
+  lda #$0b
+  sta $d021
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
+  nop
